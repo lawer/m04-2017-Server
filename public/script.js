87207da023125ea7e97740ced991eac7d1aa7808
Carles Gonzalez <carlesgm@gmail.com>
2017-08-08 20:54:46 +0200

    - Add and delete - Icons

--- public/script.js
+++ public/script.js
@@ -1,90 +1,4 @@
-// Code goes here
-
-function doTransform(xml, xsl) {
-    // code for IE
-    if (window.ActiveXObject) {
-        ex = xml.transformNode(xsl);
-        return ex;
-    }
-    // code for Chrome, Firefox, Opera, etc.
-    else if (document.implementation && document.implementation.createDocument) {
-        xsltProcessor = new XSLTProcessor();
-        xsltProcessor.importStylesheet(xsl);
-        resultDocument = xsltProcessor.transformToFragment(xml, document);
-        return resultDocument;
-    }
-
-}
-
-$.fn.renderXSLT = function renderXSLT(dataUrl, xslUrl) {
-    var target = this;
-
-    var xmlPromise = $.get({
-        url: dataUrl,
-        dataType: 'xml',
-    });
-
-    var xslPromise = $.get({
-        url: xslUrl,
-        dataType: 'xml',
-    });
-
-    return Promise
-        .all([xmlPromise, xslPromise])
-        .then(function (values) {
-            console.log(values[0]);
-            console.log(values[1]);
-
-            target.html(doTransform(values[0], values[1]));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-};
-
-$.fn.renderEJS = function renderEJS(jsonUrl, ejsUrl) {
-    var target = this;
-
-    const promises = [
-        $.get({
-            url: jsonUrl,
-        }),
-        $.get({
-            url: ejsUrl
-        }),
-    ];
-
-    return Promise
-        .all(promises)
-        .then(function (values) {
-            console.log(values[0]);
-            var html = ejs.render(values[1], values[0], "comics.ejs");
-
-            target.html(html);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-};
-
-
 $(document).ready(function () {
-    /*var urlBase = "api/";
-    var resource = "characters";
-
-    var urlXml = `${urlBase}${resource}/?format=xml`;
-    var urlXsl = "comics.xslt";
-
-    $("#comics").renderXSLT(urlXml, urlXsl)
-
-    var urlJSON = `${urlBase}${resource}/?format=json`;
-    var urlXsl = "comics.xslt";
-    var urlEjs = "comics.ejs";
-
-    //$("#comics").renderXSLT(urlJSON, urlXsl, "json");
-
-    $("#comics").renderEJS(urlJSON, urlEjs);*/
-
     var app = new Vue({
         el: '#comics',
 
@@ -92,16 +6,32 @@ $(document).ready(function () {
             characters: []
         },
 
-        created() {
-            $.getJSON("api/characters")
-                .then(response => {
-                    //console.log(response);
-                    // JSON responses are automatically parsed.
-                    this.characters = response.characters
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                })
-        }
+        methods: {
+            showCharacters: function () {
+                this.$http.get("api/characters")
+                    .then(response => {
+                        console.log(response);
+                        this.characters = response.body.characters;
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    });
+            },
+            deleteCharacter(character) {
+                console.log(`Deleting ${character}`);
+                this.$http.delete('api/characters/' + character.id)
+                    .then(data => {
+                        this.showCharacters();
+                    });
+            },
+            modifyCharacter(character) {
+                console.log(`Modifying ${character}`);
+            }
+
+        },
+
+        created: function () {
+            this.showCharacters();
+        },
     });
 });
